055c1cddc52ae8c53ab6f04e255d3c46d02f00db
vuhah <vuuu0412@gmail.com>
2022-07-31 13:47:31 +0700

    folder front-end structure

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -1,4 +1,6 @@
-import 'styles/index.scss';
+import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/index.scss'
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,7 +1,10 @@
+
 export default function Home() {
   return (
-    <div>
-      
+    <div className="a">
+      dddddfffffffffffffffffffffff
+      dfgdfg
+      fjf
     </div>
   )
 }
