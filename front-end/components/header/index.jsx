4198ff66d8b52b1c97f67f209dcd378b960a7567
vuhah <vuuu0412@gmail.com>
2022-08-03 07:07:01 +0700

    x

--- front-end/components/header/index.jsx
+++ front-end/components/header/index.jsx
@@ -1,10 +1,51 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Image from "next/image";
+import {Row, Col} from 'react-bootstrap'
+function Header() {
 
-function Header(){
-   return(
-      <div>
-         aaaaa
-      </div>
-   )
+  return (
+    <div>
+      <Navbar className="navbar" collapseOnSelect expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <Image
+              src="/brand-sunhair-removebg-preview.png"
+              alt="logo"
+              width={130}
+              height={60}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#pricing">Inside SunHair</Nav.Link>
+              <NavDropdown title="Shop" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+        <div className='recommended-panel'>ddddddddddddd</div>
+      </Navbar>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- front-end/pages/index.jsx
+++ front-end/pages/index.jsx
@@ -1,13 +1,21 @@
-import Footer from "../components/footer"
-import Header from "../components/header"
-import Container from "../components/container"
+import Header from "../components/header";
+import Container from "../components/container";
+import Footer from "../components/footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
-      <Header/>
-      <Container/>
-      <Footer/>
-    </div>
-  )
+    <>
+      <Head>
+        <title>Sun Hair Company</title>
+        <link rel="icon" type="image/x-icon" href="/logo.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div>
+        <Header />
+        <Container />
+        <Footer />
+      </div>
+    </>
+  );
 }
